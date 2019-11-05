527a411e8be30a5e6f4f3c4d1c223df529589753
Michiru Niibo <michiruniibo@MichiruMBA-2.local>
2019-11-05 19:27:35 +0900

    create script.js and add canvas

---
+++ script.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("topcanvas");
+// var tcContext = canvas.getContext("2d");
+
+canvas.width = 960;
+canvas.height = 300;
+canvas.style.backgroundColor = "#ff420e";
